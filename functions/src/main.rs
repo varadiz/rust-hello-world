e2a2fcb2be22daf470fbb42a42b0077fbb95cc94
Zoltan Varadi <contact@data-i-consulting.com>
2023-12-13 02:02:02 +0900

    expression

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    print_labeled_measurement(5, 'h');
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    // expression NOTE: no semicolon
+    x + 1
 }
